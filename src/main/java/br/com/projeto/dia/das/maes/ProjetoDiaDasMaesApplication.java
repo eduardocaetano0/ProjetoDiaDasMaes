d581efe08cf2b2f8804ef68ee1ad36d951c8658f
eduardocaetano0 <eduardocaetano110@gmail.com>
2025-05-08 23:52:11 -0300

    first commit

---
+++ src/main/java/br/com/projeto/dia/das/maes/ProjetoDiaDasMaesApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.dia.das.maes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDiaDasMaesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDiaDasMaesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/dia/das/maes/ProjetoDiaDasMaesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.dia.das.maes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDiaDasMaesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
