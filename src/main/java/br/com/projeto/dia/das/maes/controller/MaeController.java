2b9531a45d081583227525ae5bc68c971fb1cd28
eduardocaetano0 <eduardocaetano110@gmail.com>
2025-05-10 00:05:22 -0300

    Cria entidade no banco de dados, apartir do MaeModel

--- src/main/java/br/com/projeto/dia/das/maes/controller/MaeController.java
+++ src/main/java/br/com/projeto/dia/das/maes/controller/MaeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class MaeController {
 
     @GetMapping("/home")
         public String home() {
